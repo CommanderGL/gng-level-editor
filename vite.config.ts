b180dcaf77616b027dc9c7623e7574598a614562
CrossScar <crossscar2@outlook.com>
2023-11-05 10:35:22 -0800

    Migrate to NodeJS and add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,37 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'GNG Level Editor',
+        short_name: 'Level Editor',
+        description: 'A basic level editor for Grab\'n\'Go!',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   base: '/gng-level-editor/'
-})
+});
